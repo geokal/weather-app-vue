1ea23d607090aea4138b8fa0b7084cc3e675905a
Giorgos Kal <gkalpak@fogus.gr>
2023-12-05 08:00:46 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ export default createStore({
   actions: {
     async fetchWeatherData({ commit }, city) {
       try {
-        const apiKey = 'ef61d027a1f013aae3caa9659b7763a4'; // Replace with your API key
+        const apiKey = 'xxxxxxxxxxxxxxx'; // Replace with your API key
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         const response = await axios.get(apiUrl);
         commit('setWeatherData', response.data);
